f856bd89e132b6d2df3ea7937a97ca9ee3c29d69
Hariharan-thevar <harithevar65@gmail.com>
2025-11-17 17:06:14 +0530

    Refactor header structure and classes

--- header.php
+++ header.php
@@ -1,18 +1,13 @@
-<?php
-// header.php
-?>
-<header class="bg-light shadow-sm">
+<?php ?>
+<header class="site-header">
   <div class="container d-flex align-items-center justify-content-between py-2">
-    <a href="index.php" class="d-flex align-items-center text-decoration-none">
-      <img src="assets/images/logo.png" alt="SIWS Logo" style="height:56px; margin-right:12px;">
-      <div>
-        <strong>SIWS</strong><br>
-        <small>N.R. Swamy College of Commerce &amp; Economics</small>
-      </div>
+    <a href="index.php" class="brand text-decoration-none">
+      <div class="brand-logo">SIWS</div>
+      <div class="brand-text">N.R. Swamy College of Commerce & Economics</div>
     </a>
-
     <nav>
       <ul class="nav">
+        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
         <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
         <li class="nav-item"><a class="nav-link" href="programs.php">Academics</a></li>
         <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>

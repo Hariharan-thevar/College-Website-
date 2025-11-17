0503cff890d87368e285d110ea5dc9fa45893c83
Hariharan-thevar <harithevar65@gmail.com>
2025-11-17 17:00:54 +0530

    Update about page content and structure

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
-<main class="container mt-4">
-  <h1>About SIWS College</h1>
-  <p>This template contains a short about page. Replace this with the college's real mission and history content or your own project text.</p>
-  <a href="index.php">Back to Home</a>
+<main class='container mt-4'>
+<h1>About SIWS College</h1>
+<p>This is a colourful, image-free PHP template.</p>
+<a href='index.php'>Back to Home</a>
 </main>
 <?php include 'footer.php'; ?>
